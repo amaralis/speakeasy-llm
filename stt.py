cac42f0761d10b4d2de5ab498e6f6a825ca9550f
Francisco Amaral <guedesmagalhaes@gmail.com>
2023-12-03 21:03:58 +0000

    restructure folders

--- stt.py
+++ stt.py
@@ -4,6 +4,6 @@ warnings.simplefilter("ignore")
 
 model = whisper.load_model("tiny")
 
-result = model.transcribe(audio="./sample2.mp3")
+result = model.transcribe(audio="./audio-files/sample2.mp3")
 
 print(result["text"])

ce91359fe8a5e4584683dcb694d80c782e9a29cf
Francisco Amaral <guedesmagalhaes@gmail.com>
2023-12-10 00:09:49 +0000

    minor cleanup

---
+++ speakeasyLlm.py
@@ -0,0 +1,25 @@
+from LLMConnector import LLMConnector
+from protocols.promptable import Promptable
+from llms.LMStudio import LMStudio
+from record import record
+from stt import speechToText
+from tts import play
+
+llm: Promptable = LMStudio()
+
+def startPrompting(model=llm):
+    connector = LLMConnector(model)
+    
+    # Start and stop recording with space bar
+    record('space', './audio-files/recordedSample.wav')
+
+
+    prompt = speechToText("./audio-files/recordedSample.wav", './transcriptions/transcription.txt')
+    print("Recorded speech: ", prompt)
+
+    # Send the prompt converted to text to the LLM
+    response = connector.sendPrompt(model, prompt)
+    print(response)
+
+    # Run text to speech on the text response
+    play(response)
